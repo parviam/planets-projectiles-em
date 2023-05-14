f4dd7219cc91855b96df8d2585423825b3a7f63e
Parv <parviam@gmail.com>
2023-05-14 15:03:56 -0400

    new atmospheric model, some debugging for c_d

--- orbital.py
+++ orbital.py
@@ -6,6 +6,7 @@ print("done")
 
 print("importing math...", end='')
 import math
+import random
 print("done")
 
 print("importing matplotlib...", end = '')
@@ -26,27 +27,83 @@ def g(m_s, r_s, h):     #calculates acceleration due to gravity at some height a
     #h - height of object above the surface (m)
     return np.array([0, 0, -G*m_s/(r_s+h)**2])   #from newton's law of universal gravitation
 
-def temp(h):        #calculates temperature in kelvin ==>BROKEN
-    return 210.372 #avg temp placeholder
-    if h > 7000:
-        return -23.4-0.00222*h + 273.15
-    return -31 - 0.000998 * h + 273.15
-
-def v_sound(h, t):     #calculates velocity of sound
-    coefficient = 1.289*0.1889/44.01
-    return math.sqrt(coefficient * t)
+def temp(h):        #calculates temperature  MARS ONLY 
+    h_km = h/1000 #height in km
+    if h_km > 300:
+        raise ValueError("height is higher than 3000!")
+    if h_km > 120:
+        zet = (h_km-120)*(3.38951 + 120)/(3389.51 + h_km)
+        return 200 - 72.225 * math.exp(-0.0195 * zet)
+    if h_km > 105:
+        return 203.25 - 0.65 * h_km
+    if h_km > 95:
+        return 282 - 1.40 * h_km
+    if h_km > 84:
+        return 149
+    if h_km > 75:
+        return -61 + 2.5 * h_km
+    if h_km > 66:
+        return 314 - 2.5 * h_km
+    if h_km > 55:
+        return 106.1 + 0.65 * h_km
+    if h_km > 48:
+        return 271.10 - 2.35 * h_km
+    if h_km > 39:
+        return 158.30
+    if h_km > -8:
+        return 228.5 - 1.8 * h_km
+    else:
+        raise ValueError("height is less than -8!! and is actually", h)
+def pressure(h):        #calculates pressure at height ONLY MARS 
+    h_km = h/1000 #height in km
+    if h_km > 300:
+        raise ValueError("height is higher than 3000!")
+    if h_km > 200:
+        return math.exp(-4.83452E-11*h_km**5 + 6.96178E-08*h_km**4 - 4.03197E-05*h_km**3 + 0.0117655*h_km**2 - 1.76494*h_km + 93.67154)
+    if h_km > 120:
+        return math.exp(-4.18520E-10*h_km**5 + 3.45846E-07*h_km**4 - 1.13352E-04*h_km**3 + 0.0188613*h_km**2 - 1.71718*h_km + 61.10381)
+    if h_km > 105:
+        return 0.00169282 * (135/(135 - 0.65*(h_km-105)))**(-19.435/0.65)
+    if h_km > 95:
+        return 0.00666032 * (149/(149 - 1.40*(h_km-95)))**(-19.435/1.40)
+    if h_km > 84:
+        return 0.0279653*math.exp(-19.435*(h_km-84)/149.00)
+    if h_km > 75:
+        return 0.0998430 * (126.50/(126.50 +2.50*(h_km-75)))**(19.435/2.50)
+    if h_km > 66:
+        return 0.356464 * (149.0/(149.0 - 2.50*(h_km-66)))**(-19.435/2.50)
+    if h_km > 55:
+        return 1.55091 * (141.85/(141.85 + 0.65*(h_km-55)))**(19.435/0.65)
+    if h_km > 48:
+        return 3.84305 * (158.30/(158.35 - 2.35*(h_km-48)))**(19.435/-2.35)
+    if h_km > 39:
+        return 11.6025 * math.exp(-19.435*(h-39)/158.30)
+    if h_km > -8:
+        return 610.5 * (228.50/(228.50-1.80*h_km))**(-19.435/1.8)
+    raise ValueError("height is less than -8!")
+def v_sound(t):     #calculates velocity of sound ONLY MARS
+    heat_r = 0.000001409*(t**2) - 0.001192*t + 1.5175       #specific heat ratio
+    gas_const = 191.181     #specific gas constant of mars
+    return math.sqrt(heat_r*gas_const*t)
 
 def rho(h, t):     #calculates volume density of air MARS ONLY
-    return 0.699*math.e**(-0.0009*h)/(0.1921*t)  #temp var for efficiency
-
-def viscosity(h, t):      #calculates viscosity of air MARS ONLY
-    coefficient = 0.01480 *(0.555*527.67+240)       #temp var for efficiency
-    coefficient /= 0.555*(5/9*t) + 240
-    coefficient *= (5/9*t/527.67)**(3/2)
-    return coefficient
+    h_km = h/1000
+    if h_km > 300:
+        raise ValueError("height > 300!!")
+    if h_km > 200:
+        return math.exp(2.65472E-11*h_km**5 - 2.45558E-08*h_km**4 + 6.31410E-06*h_km**3 + 4.73359E-04*h_km**2 - 0.443712*h_km + 23.79408)
+    if h_km > 120:
+        return math.exp(-2.55314E-10*h_km**5 + 2.31927E-07*h_km**4 - 8.33206E-05*h_km**3 + 0.0151947*h_km**2 - 1.52799*h_km + 48.69659)
+    p = pressure(h)
+    return p/(191.181*t)
+def viscosity(t):      #calculates viscosity of air based on 100% CO2 MARS ONLY
+    mu_0 = 1.370e-5 #viscosity constant 
+    s_c = 222 #sutherland's constant
+    t_0 = 273 #temperature cosntant, kelvin ratio
+    return mu_0*(t/t_0)**(3/2)*((t_0 + s_c)/(t+s_c))    #sutherland's law
         
-def c_d(rho_a,v_mag,r_b,h,t):    #calculates coefficient of drag using piecewise
-    mu = viscosity(h,t)       #viscosity of air at height
+def c_d(rho_a,v_mag,r_b,t):    #calculates coefficient of drag using piecewise
+    mu = viscosity(t)       #viscosity of air at height
     reynolds = rho_a*v_mag*2*r_b/mu     #reynolds number
     if reynolds < 0.2:
         return 24/reynolds
@@ -54,15 +111,17 @@ def c_d(rho_a,v_mag,r_b,h,t):    #calculates coefficient of drag using piecewise
     if reynolds < 2*10**3:
         return coefficient
 
-    a_waves = v_sound(h, t)    #velocity of sound at height
+    a_waves = v_sound(t)    #velocity of sound at temp
     mach = v_mag/a_waves    #mach number, below only works for mach < 1
-    return coefficient*(1-0.445*mach + 4.84*mach**2-9.73*mach**3+6.93*mach**4)/math.sqrt(1+1.2*mach*coefficient)
+    if mach < 1:
+        return coefficient*(1-0.445*mach + 4.84*mach**2-9.73*mach**3+6.93*mach**4)/math.sqrt(1+1.2*mach*coefficient)
+    return 0.005
 
 def drag_a(v, r_b, m_b, h):        #calculates drag acceleration in Newtons
     t = temp(h)     #temperature of air at height
     v_mag = mag(v)  #magnitude of v
     rho_a = rho(h, t)  #density of air
-    a = v_mag**2*math.pi*r_b**2*rho_a*c_d(rho_a,v_mag,r_b,h,t)/(2*m_b)
+    a = v_mag**2*math.pi*r_b**2*rho_a*c_d(rho_a,v_mag,r_b,t)/(2*m_b)
     return -a*v/v_mag        
 
 def accel(m_s, r_s, h, v, r_b, m_b):    #determines acceleration of object
@@ -105,11 +164,11 @@ def get_initial_v(s_0, m_s, r_s, h_a, r_b, m_b, dt):
         a = accel(m_s,r_s,s[2],v, r_b, m_b)    #current acceleration variable (m/s^2)
         s = s + delta_x(a, v, dt)   #changes position with displacement calculation
         v = new_v(v, a, dt)     #changes velocity with displacement calculation
+        #v_store = np.append(v_store, v[2])
         t_count += dt       #printing feedback system
-        v_store = np.append(v_store, v[2])
-        if v[2] > 10**25:
-            break
-    return v_store
+        if t_count % 10 < dt:
+            print(v[2], "m/s vertical at", s[2], "m above ground at", t_count, "s")
+    return v
 
 #Testing Suite
 
@@ -123,7 +182,7 @@ h_a_mars = 230000 #height of atmosphere on mars (m)
 r_b = 1 #radius of ball (m)
 m_b = 1 #mass of ball (kg)
 
-pltget_initial_v(s_0,m_mars,r_mars,h_a_mars,r_b,m_b,dt)
+print(get_initial_v(s_0,m_mars,r_mars,h_a_mars,r_b,m_b,dt)[2])
 
 
 
